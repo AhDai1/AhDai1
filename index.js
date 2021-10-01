9d8b21984401ea7dd2938b9767a89640c71c06fd
AhDai1 <53222937+AhDai1@users.noreply.github.com>
2021-10-01 16:37:02 +0000

    Update index.js

--- index.js
+++ index.js
@@ -23,14 +23,12 @@ const readme = `\
 ### My GitHub Contributions    
 ![](https://raw.githubusercontent.com/AhDai1/AhDai1/main/assets/github-contribution-grid-snake.svg)          
 ### About me      
-[![Angola peng's GitHub stats](https://github-readme-stats.vercel.app/api?username=phh95&show_icons=true&theme=radical)](https://github.com/anuraghazra/github-readme-stats)
-![Angola peng's Most used languages](https://github-readme-stats.vercel.app/api/top-langs/?username=phh95&layout=compact&hide_border=true&langs_count=10)
-I'm angola, a new-media editor.    
-- 🔭 I’m currently striving for a better life in
-- 🤔 My blog 👉
-- 💬 Ask me some questions about Effeciency Tools 
-- 📫 My E-mail:         
-- 🔧 My Power Tools: </br>   
+[![Angola peng's GitHub stats](https://github-readme-stats.vercel.app/api?username=AhDai1&show_icons=true&theme=radical)](https://github.com/anuraghazra/github-readme-stats)
+![Angola peng's Most used languages](https://github-readme-stats.vercel.app/api/top-langs/?username=AhDai1&layout=compact&hide_border=true&langs_count=10)
+
+- 🤔 My blog 👉 ahdai1.github.io
+- 📫 My E-mail: 1305377980@qq.com        
+
 ![](https://img.shields.io/badge/%E5%86%99%E4%BD%9C%E5%B7%A5%E5%85%B7-VS%20Code-blue)     
 ![VS Code](https://img.shields.io/badge/%E5%86%99%E4%BD%9C%E5%B7%A5%E5%85%B7-VS%20Code-blue)     
 ![Visual Studio Code](https://img.shields.io/badge/Visual_Studio_Code-007ACC?style=flat-square&logo=Visual-Studio-Code&logoColor=white)       
