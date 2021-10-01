dfe455bec25a74a8d905adac75bfbb2cb48e9f9f
AhDai1 <53222937+AhDai1@users.noreply.github.com>
2021-10-01 15:10:09 +0000

    Update index.js

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ function generateProgressBar() {
 
 const readme = `\
 ### Hi there 👋
-⏳ Year progress
+Year progress
 ![Progress Bar CI](https://github.com/AhDai1/AhDai1/workflows/Progress%20Bar%20CI/badge.svg)
 
 ![](https://raw.githubusercontent.com/AhDai1/AhDai1/main/assets/github-contribution-grid-snake.svg)
