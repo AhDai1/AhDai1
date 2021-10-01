48465a33e3f6f71a20bebde862f59161ccf33023
AhDai1 <53222937+AhDai1@users.noreply.github.com>
2021-10-01 14:51:06 +0000

    Update index.js

--- index.js
+++ index.js
@@ -12,16 +12,3 @@ function generateProgressBar() {
       '▁'.repeat(progressBarCapacity - passedProgressBarIndex)
     return `{ ${progressBar} }`
 }
-
-const readme = `\
-### Hi there 👋
-
-⏳ Year progress ${progressBarOfThisYear} ${(progressOfThisYear * 100).toFixed(2)} %
-
----
-
-⏰ Updated on ${new Date().toUTCString()}
-![Progress Bar CI](https://github.com/AhDai1/AhDai1/workflows/Progress%20Bar%20CI/badge.svg)\
-`
-
-console.log(readme)
