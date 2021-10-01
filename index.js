207fea6c2388c941392afa2b88780526560f127e
AhDai1 <53222937+AhDai1@users.noreply.github.com>
2021-10-01 14:43:34 +0000

    Update index.js

--- index.js
+++ index.js
@@ -19,3 +19,9 @@ const readme = `\
 ⏳ Year progress ${progressBarOfThisYear} ${(progressOfThisYear * 100).toFixed(2)} %
 
 ---
+
+⏰ Updated on ${new Date().toUTCString()}
+![Progress Bar CI](https://github.com/AhDai1/AhDai1/workflows/Progress%20Bar%20CI/badge.svg)\
+`
+
+console.log(readme)
